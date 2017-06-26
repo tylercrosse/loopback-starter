89f4fd94ed59ced99c0dfdf618983982a7aa39cb
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-06-25 18:23:18 -0700

    Add remote-method and validation to Product

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,65 @@
 'use strict';
 
 module.exports = function(Product) {
+  /**
+   * Return true if input is larger than zero
+   * @param  {number} quantity Number to validate
+   * @return {boolean}         True if input is larger than zero
+   */
+  const validQuantity = quantity => Boolean(quantity > 0);
 
+  /**
+   * Buy this product
+   * @param {number} quantity Number of products to buy
+   * @param {Function(Error, object)} callback
+   */
+
+  Product.prototype.buy = function(quantity, callback) {
+    if (!validQuantity(quantity)) {
+      return callback(`Invalid quantity ${quantity}`);
+    }
+    const result = {
+      status: `You bought ${quantity} product(s)`,
+    };
+    callback(null, result);
+  };
+
+  // Validate minimal length of the name
+  Product.validatesLengthOf('name', {
+    min: 3,
+    message: {
+      min: 'Name should be at least 3 characters',
+    },
+  });
+
+  // Validate name to be unique
+  Product.validatesUniquenessOf('name');
+
+  const positiveInteger = /^[0-9]*$/;
+
+  const validatePositiveInteger = function(err) {
+    if (!positiveInteger.test(this.price)) {
+      err();
+    }
+  };
+
+  Product.validate('price', validatePositiveInteger, {
+    message: 'Price should be a positive integer',
+  });
+
+  function validateMinimalPrice(err, done) {
+    const price = this.price;
+
+    process.nextTick(() => {
+      const minimalPriceFromDB = 99;
+      if (price < minimalPriceFromDB) {
+        err();
+      }
+      done();
+    });
+  }
+
+  Product.validateAsync('price', validateMinimalPrice, {
+    message: 'Price should be higher than the normal price in the db',
+  });
 };
